fdfd89163457da48574082129288d28b0edb483a
Omar Garcia <omar.garcia@omargl.net>
2026-05-24 15:23:12 -0600

    add hi.c to print ASCII values of characters H, I, and !

---
+++ 08_score/hi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+
+    // Print the ASCII values of the characters
+    printf("%i %i %i\n", c1, c2, c3);
+}
