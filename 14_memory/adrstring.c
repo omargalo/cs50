1fe20870e0053b02dbeceb282397cc24bf0c761c
Omar Garcia <omar.garcia@omargl.net>
2026-05-27 08:34:54 -0600

    refactor adrstring.c to uncomment main function and demonstrate character pointer usage

--- 14_memory/adrstring.c
+++ 14_memory/adrstring.c
@@ -12,10 +12,20 @@
     printf("%p\n", &s[3]);
 } */
 
-int main(void)
+/* int main(void)
 {
     // string is just a char *, so we can use char * instead of string
     char *s = "HI!";
 
     printf("%s\n", s);
+} */
+
+int main(void)
+{
+    char *s = "HI!";
+
+    printf("%c\n", *s);
+    // s + 1 is the same as &s[1]
+    printf("%c\n", *(s + 1));
+    printf("%c\n", *(s + 2));
 }
