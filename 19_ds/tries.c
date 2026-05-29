5094813053a5cff5ddae000113fd398fcc436c83
Omar Garcia <omar.garcia@omargl.net>
2026-05-29 12:11:02 -0600

    add tries.c to implement a trie data structure for efficient prefix-based searches

---
+++ 19_ds/tries.c
@@ -0,0 +1,9 @@
+// Tries are a type of search tree used to store associative data structures.
+// Each node in a trie represents a common prefix of some keys,
+// and the edges represent the characters in the keys.
+// Tries are often used for tasks such as autocomplete and spell checking.
+
+// Tries uses a lot of memory, as each node can have many children
+// (up to the number of possible characters).
+// However, they can be very efficient for certain types of searches,
+// especially when the keys have common prefixes.
