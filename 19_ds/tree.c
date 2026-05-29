5e15853d3131720c63463712195aa45e1985b03b
Omar Garcia <omar.garcia@omargl.net>
2026-05-29 11:33:40 -0600

    add tree.c to implement a binary tree search function

---
+++ 19_ds/tree.c
@@ -0,0 +1,21 @@
+/*
+bool search(node *tree, int number)
+{
+    if (tree == NULL)
+    {
+        return false;
+    }
+    else if (number < tree->number)
+    {
+        return search(tree->left, number);
+    }
+    else if (number > tree->number)
+    {
+        return search(tree->right, number);
+    }
+    else
+    {
+        return true;
+    }
+}
+*/
